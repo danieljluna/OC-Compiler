354d1d84e5e12c82c8467b7fa34ab2752f510535
Daniel Luna <danieljluna0@gmail.com>
2016-04-07 11:19:19 -0700

    Should compile now

--- compiler/stringsetMain.cpp
+++ compiler/stringsetMain.cpp
@@ -9,23 +9,8 @@ using namespace std;
 
 #include "stringset.h"
 
+/*
 int main (int argc, char** argv) {
-   set_execname (argv[0]);
-   for (int argi = 1; argi < argc; ++argi) {
-      char* filename = argv[argi];
-      string command = CPP + " " + filename;
-      printf ("command=\"%s\"\n", command.c_str());
-      FILE* pipe = popen (command.c_str(), "r");
-      if (pipe == NULL) {
-         syserrprintf (command.c_str());
-      }else {
-         cpplines (pipe, filename);
-         int pclose_rc = pclose (pipe);
-         eprint_status (command.c_str(), pclose_rc);
-         if (pclose_rc != 0) set_exitstatus (EXIT_FAILURE);
-      }
-   }
-   
    vector<string> args (&argv[1], &argv[argc]);
    for (const string& arg: args) {
       const string* str = intern_stringset (arg.c_str());
@@ -35,6 +20,4 @@ int main (int argc, char** argv) {
    dump_stringset (cout);
    return EXIT_SUCCESS;
 }
-
-
-
+*/
